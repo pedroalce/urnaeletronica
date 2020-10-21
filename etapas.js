a6d5b6252f41427ec517d7f5342314c4741e7921
pedroalce <pedro.alce@al.resilia.com.br>
2020-10-21 11:58:33 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Justin Bieber',
+                partido:  'Def Jam Recordings',
+                fotos: [
+                    {url:'38111.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Jaden Smith',
+                partido: 'Roc Nation Records',
+                fotos: [
+                    {url: '77222.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+        ]
+    },
+       {
+
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Michael Jackson',
+                partido: 'Sony',
+                vice: 'Justin Timberlake',
+                fotos: [
+                    {url: '99.jpeg', legenda: 'Prefeito'},
+                    {url: '99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '89',
+                nome: 'Prince',
+                partido: 'Warner Records',
+                vice: 'Bruno Mars',
+                fotos: [
+                    {url: '89.jpeg', legenda: 'Prefeito'},
+                    {url: '89_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+]
